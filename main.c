53cd8226fd085b660de7f31158663f7e967ac914
Michael Lee <michaelleestuy@gmail.com>
2017-11-21 11:51:39 -0500

    trash

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <string.h>
+
+char ** parse_args( char * line){
+  char ** args = malloc(sizeof(char*) * 100);
+  int i = 0;
+
+  while(line){
+    args[i] = line;
+    // printf("given line: %s\n", line);
+    strsep(&line, " ");
+    // printf("after strsep new line: %s\n", line);
+    i++;
+  }
+  return args;
+}
+
+int main(){
+  char line[100];
+  scanf("%s", line);
+  parse_args(line);
+  return 0;
+
+
+
+
+
+}

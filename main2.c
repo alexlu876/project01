f2892576a3d677db871c24af06ae0eb26253ef89
saltonbench <jlin22@stuy.edu>
2017-12-03 22:33:55 -0500

    update headers

--- main2.c
+++ main2.c
@@ -6,9 +6,25 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-char ** parse_args(char * line);//parse the line by space and return the array of the individual strings
-void fork_and_run(char ** args,int x,int y);//forks and executes the command given
-void do_everything(char * line);//the entire program
+/*
+/======== char ** parse_args() ==========
+Input: char * line Returns: Array of the individual strings, which were separated by spaces Separates the string line by ' ' and returns an array of the individual strings
+
+/======== void fork_and_runn() ==========
+Input: char ** args Forks the current process, while the parent executes the command given in args, and the child waits for the parent to do so
+/======== char ** fork_and_run() ==========
+Input: char ** args
+int x
+int y
+Does what fork_and_runn does, but sets 1 to stdout if 1 isn't stdout, and sets 0 to stdin if 0 isn't stdin
+/======== void do_everything() ==========
+Input: char * line
+If there is a ;, will recursively execute the commands separated by the ; If there is a >, will redirect the stdout produced from what comes before the > to a created file with the name of what is after the >
+If there is a <, will redirect the stdin from the file, which is named what comes after the < to either the command, which comes before the <, or to the shell program, main2.c, if nothing comes before the <
+If there is a |, will create a pipe, which redirects the stdout of what comes before the | to the stdin of what comes after the |
+/======== int main() ==========
+Prints out "The Shell of Justice$" after every execution of command
+Exits program if exit is called If cd <DIRECTORY> is called, changes directory to If the above two conditions arent met, calls do_everything() to execute the command(s) called*/
 
 char ** parse_args( char * line){
   if(line[strlen(line)-1] == ' '){
